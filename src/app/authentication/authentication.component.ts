832d7e5afd59aebfef35c359177ef1265f8a3e45
Laurin Herteux <156675458+Herteux94@users.noreply.github.com>
2024-08-07 16:31:59 +0200

    added imports in authentication.component.ts
    imported:
    
    - LoginComponent
    - SignUpComponent,
    - ChooseAvatarComponent,
    - SendResetPwMailComponent,
    - ResetPwComponent

--- src/app/authentication/authentication.component.ts
+++ src/app/authentication/authentication.component.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { LoginComponent } from './login/login.component';
+import { ChooseAvatarComponent } from './choose-avatar/choose-avatar.component';
+import { SendResetPwMailComponent } from './send-reset-pw-mail/send-reset-pw-mail.component';
+import { ResetPwComponent } from './reset-pw/reset-pw.component';
 
 @Component({
   selector: 'app-authentication',
   standalone: true,
-  imports: [],
+  imports: [
+    LoginComponent,
+    SignUpComponent,
+    ChooseAvatarComponent,
+    SendResetPwMailComponent,
+    ResetPwComponent
+
+  ],
   templateUrl: './authentication.component.html',
   styleUrl: './authentication.component.scss'
 })
