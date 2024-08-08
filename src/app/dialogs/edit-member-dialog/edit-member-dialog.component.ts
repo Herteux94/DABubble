d7f61828391251ef18d45b13fc6e09d06d0b78b8
RobinWestmeier <146763348+RobinWestmeier@users.noreply.github.com>
2024-08-08 13:09:01 +0200

    added edit member dialog

--- src/app/dialogs/edit-member-dialog/edit-member-dialog.component.ts
+++ src/app/dialogs/edit-member-dialog/edit-member-dialog.component.ts
@@ -1,12 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, NgForm, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-member-dialog',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './edit-member-dialog.component.html',
   styleUrl: './edit-member-dialog.component.scss'
 })
 export class EditMemberDialogComponent {
+  
+  contactData = {
+    name: ''
+  };
+
+  onSubmit(ngForm: NgForm) {
+    
+  }
 
 }
