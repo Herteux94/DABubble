d7ce17b7ba88e9773a8698b2508c637e3728a4a1
RobinWestmeier <146763348+RobinWestmeier@users.noreply.github.com>
2024-08-08 09:21:57 +0200

    Added Workspace for shared working

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { MainContentComponent } from './main-content/main-content.component';
 import { AuthenticationComponent } from './authentication/authentication.component';
+import { WorkspaceComponent } from './workspace/workspace.component';
 
 export const routes: Routes = [
+    { path: '', component: WorkspaceComponent },
     { path: 'main-content', component: MainContentComponent },
     { path: 'authentication', component: AuthenticationComponent }
 
