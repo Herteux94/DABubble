178ba2c256eb651a497cadee78644f574ab883d7
Kytez <161442764+Kytez@users.noreply.github.com>
2024-08-08 08:39:00 +0200

    added routes for creating parallel

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { MainContentComponent } from './main-content/main-content.component';
+import { AuthenticationComponent } from './authentication/authentication.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'main-content', component: MainContentComponent },
+    { path: 'authentication', component: AuthenticationComponent }
+
+];
