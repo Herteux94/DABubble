a2119e7169efa6d78e49b0e200d03143eb969fd6
RobinWestmeier <146763348+RobinWestmeier@users.noreply.github.com>
2024-08-08 09:51:42 +0200

    fixed routing for dialog-test

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,12 @@ import { Routes } from '@angular/router';
 import { MainContentComponent } from './main-content/main-content.component';
 import { AuthenticationComponent } from './authentication/authentication.component';
 import { WorkspaceComponent } from './workspace/workspace.component';
-import { ChannelMemberDialogComponent } from './dialogs/channel-member-dialog/channel-member-dialog.component';
+import { DialogTestComponent } from './dialog-test/dialog-test.component';
 
 export const routes: Routes = [
     { path: '', component: WorkspaceComponent },
     { path: 'main-content', component: MainContentComponent },
     { path: 'authentication', component: AuthenticationComponent },
-    { path: 'dialogs', component: ChannelMemberDialogComponent }
+    { path: 'dialogs', component: DialogTestComponent }
 
 ];
