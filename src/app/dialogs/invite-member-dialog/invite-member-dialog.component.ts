81b8da4f35b3c13d21d66072ead87a4871627467
RobinWestmeier <146763348+RobinWestmeier@users.noreply.github.com>
2024-08-08 11:20:16 +0200

    updated old dialog and added invite-member-dialog

--- src/app/dialogs/invite-member-dialog/invite-member-dialog.component.ts
+++ src/app/dialogs/invite-member-dialog/invite-member-dialog.component.ts
@@ -1,12 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, NgForm, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-invite-member-dialog',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './invite-member-dialog.component.html',
   styleUrl: './invite-member-dialog.component.scss'
 })
 export class InviteMemberDialogComponent {
 
+  contactData = {
+    name: ''
+  };
+
+  onSubmit(ngForm: NgForm) {
+    
+  }
 }
