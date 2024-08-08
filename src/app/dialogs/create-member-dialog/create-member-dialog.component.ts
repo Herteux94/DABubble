7ccff3a91c80e299b98929632b200cd38659e4eb
RobinWestmeier <146763348+RobinWestmeier@users.noreply.github.com>
2024-08-08 13:41:41 +0200

    added create member dialog

--- src/app/dialogs/create-member-dialog/create-member-dialog.component.ts
+++ src/app/dialogs/create-member-dialog/create-member-dialog.component.ts
@@ -1,12 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, NgForm, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-create-member-dialog',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './create-member-dialog.component.html',
   styleUrl: './create-member-dialog.component.scss'
 })
 export class CreateMemberDialogComponent {
+
+  contactData = {
+    name: ''
+  };
+
+  onSubmit(ngForm: NgForm) {
+    
+  }
 
 }
