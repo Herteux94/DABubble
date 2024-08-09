35f68ee44cf2c986c4989f3a8fedcfb9eef78b61
Laurin Herteux <156675458+Herteux94@users.noreply.github.com>
2024-08-09 11:29:01 +0200

    added routing for authentication component and children

--- src/app/authentication/choose-avatar/choose-avatar.component.ts
+++ src/app/authentication/choose-avatar/choose-avatar.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-choose-avatar',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './choose-avatar.component.html',
   styleUrl: './choose-avatar.component.scss'
 })
--- src/app/authentication/send-reset-pw-mail/send-reset-pw-mail.component.ts
+++ src/app/authentication/send-reset-pw-mail/send-reset-pw-mail.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-send-reset-pw-mail',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './send-reset-pw-mail.component.html',
   styleUrl: './send-reset-pw-mail.component.scss'
 })
