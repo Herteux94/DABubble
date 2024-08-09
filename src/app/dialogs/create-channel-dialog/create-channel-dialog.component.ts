82e3da27a6543402024dff7d226d20e456f31fe7
RobinWestmeier <146763348+RobinWestmeier@users.noreply.github.com>
2024-08-09 13:02:46 +0200

    added 2 dialog components

---
+++ src/app/dialogs/create-channel-dialog/create-channel-dialog.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-channel-dialog',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-channel-dialog.component.html',
+  styleUrl: './create-channel-dialog.component.scss'
+})
+export class CreateChannelDialogComponent {
+
+}
