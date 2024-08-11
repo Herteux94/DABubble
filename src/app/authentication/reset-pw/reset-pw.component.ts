c55e081061e5892cce249da3f378eafe3e425ecb
Laurin Herteux <156675458+Herteux94@users.noreply.github.com>
2024-08-11 20:05:23 +0200

    added standard inputfields
    also:
    
    - corrected authentication: standard inputfields
    - added password vaue match needed at reset-pw.component to enable Button

--- src/app/authentication/reset-pw/reset-pw.component.ts
+++ src/app/authentication/reset-pw/reset-pw.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-reset-pw',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './reset-pw.component.html',
   styleUrl: './reset-pw.component.scss'
 })
 export class ResetPwComponent {
+  inputValue: string = '';
+  inputValue2: string = '';
 
+  inputValuesMatch(): boolean {
+    return this.inputValue === this.inputValue2 && this.inputValue !== '';
+  }
 }
--- src/app/authentication/send-reset-pw-mail/send-reset-pw-mail.component.ts
+++ src/app/authentication/send-reset-pw-mail/send-reset-pw-mail.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-send-reset-pw-mail',
   standalone: true,
-  imports: [RouterModule],
+  imports: [
+    RouterModule,
+    FormsModule
+  ],
   templateUrl: './send-reset-pw-mail.component.html',
   styleUrl: './send-reset-pw-mail.component.scss'
 })
 export class SendResetPwMailComponent {
-
+  inputValue: string = '';
 }
