1d9ef3c3d6ab7fd0d4b527ae94b4b15ddf48d8af
RobinWestmeier <146763348+RobinWestmeier@users.noreply.github.com>
2024-08-11 13:07:49 +0200

    added channel-component

--- src/app/dialogs/channel-dialog/channel-dialog.component.ts
+++ src/app/dialogs/channel-dialog/channel-dialog.component.ts
@@ -1,12 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, NgForm, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-channel-dialog',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './channel-dialog.component.html',
   styleUrl: './channel-dialog.component.scss'
 })
 export class ChannelDialogComponent {
+  contactData = {
+    name: ''
+  };
 
+  onSubmit(ngForm: NgForm) {
+    
+  }
 }
