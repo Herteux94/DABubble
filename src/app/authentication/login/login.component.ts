4bb5f912b72dd0d8aa43688fdf9e3ebda427ac84
Laurin Herteux <156675458+Herteux94@users.noreply.github.com>
2024-08-12 11:01:28 +0200

    changed input styles.scss names
    inputField -> inputFieldAuth
    inputContainer -> inputContainerAuth

--- src/app/authentication/login/login.component.ts
+++ src/app/authentication/login/login.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { AuthenticationComponent } from '../authentication.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterModule],
+  imports: [
+    RouterModule,
+    AuthenticationComponent
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
