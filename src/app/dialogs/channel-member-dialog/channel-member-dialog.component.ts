ecc79c2e1d8aa6416f709e98bc93f80a158f6c4c
RobinWestmeier <146763348+RobinWestmeier@users.noreply.github.com>
2024-08-12 12:23:56 +0200

    dialogs responsive for desktop

--- src/app/dialogs/channel-member-dialog/channel-member-dialog.component.ts
+++ src/app/dialogs/channel-member-dialog/channel-member-dialog.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ScreenSizeService } from '../../services/screen-size-service.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-channel-member-dialog',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './channel-member-dialog.component.html',
   styleUrl: './channel-member-dialog.component.scss'
 })
-export class ChannelMemberDialogComponent {
+export class ChannelMemberDialogComponent implements OnInit {
 
+  mobile: boolean = false;
+
+  constructor(private screenSizeService: ScreenSizeService) {}
+
+  ngOnInit() {
+    this.screenSizeService.isMobile().subscribe(isMobile => {
+      this.mobile = isMobile;
+    });
+  }
 }
--- src/app/dialogs/edit-member-dialog/edit-member-dialog.component.ts
+++ src/app/dialogs/edit-member-dialog/edit-member-dialog.component.ts
@@ -1,22 +1,27 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule, NgForm, ReactiveFormsModule } from '@angular/forms';
+import { ScreenSizeService } from '../../services/screen-size-service.service';
 
 @Component({
   selector: 'app-edit-member-dialog',
   standalone: true,
   imports: [FormsModule, CommonModule],
   templateUrl: './edit-member-dialog.component.html',
-  styleUrl: './edit-member-dialog.component.scss'
+  styleUrl: './edit-member-dialog.component.scss',
 })
-export class EditMemberDialogComponent {
-  
+export class EditMemberDialogComponent implements OnInit {
   contactData = {
-    name: ''
+    name: '',
   };
+  mobile: boolean = false;
 
-  onSubmit(ngForm: NgForm) {
-    
+  constructor(private screenSizeService: ScreenSizeService) {}
+
+  ngOnInit() {
+    this.screenSizeService.isMobile().subscribe((isMobile) => {
+      this.mobile = isMobile;
+    });
   }
-
+  onSubmit(ngForm: NgForm) {}
 }
