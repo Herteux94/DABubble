7661442f2f53c3356b373985cb13e102ff41c746
RobinWestmeier <146763348+RobinWestmeier@users.noreply.github.com>
2024-08-12 17:04:44 +0200

    made channel dialog responsive and added transformation

--- src/app/dialogs/channel-dialog/channel-dialog.component.ts
+++ src/app/dialogs/channel-dialog/channel-dialog.component.ts
@@ -1,20 +1,55 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormsModule, NgForm, ReactiveFormsModule } from '@angular/forms';
+import { ScreenSizeService } from '../../services/screen-size-service.service';
 
 @Component({
   selector: 'app-channel-dialog',
   standalone: true,
   imports: [FormsModule, CommonModule],
   templateUrl: './channel-dialog.component.html',
-  styleUrl: './channel-dialog.component.scss'
+  styleUrl: './channel-dialog.component.scss',
 })
-export class ChannelDialogComponent {
+export class ChannelDialogComponent implements OnInit {
   contactData = {
-    name: ''
+    name: '',
   };
+  isTriggered: boolean = false;
+  isEditingName: boolean = false;
+  isEditingDescription: boolean = false;
+  channelName: string = 'Entwicklerteam';
+  channelDescription: string = 'Dieser Channel ist für alles rund um #dsfdf vorgesehen. Hier kannst du zusammen mit deinem Team Meetings abhalten, Dokumente teilen und Entscheidungen treffen.';
+  mobile: boolean = false;
 
-  onSubmit(ngForm: NgForm) {
-    
+  constructor(private screenSizeService: ScreenSizeService) {}
+
+  ngOnInit() {
+    this.screenSizeService.isMobile().subscribe((isMobile) => {
+      this.mobile = isMobile;
+    });
   }
+
+  @ViewChild('channelNameInput') channelNameInput!: ElementRef;
+  @ViewChild('channelDescriptionInput') channelDescriptionInput!: ElementRef;
+  toggleEdit() {
+    this.isEditingName = !this.isEditingName;
+    if (this.isEditingName) {
+      // Verstecke die Outline bei Bearbeitung
+      setTimeout(() => {
+        this.channelNameInput.nativeElement.focus();
+      }, 0);
+    }
+  }
+
+  toggleEditDescription() {
+    this.isEditingDescription = !this.isEditingDescription;
+    if (this.isEditingDescription) {
+      // Verstecke die Outline bei Bearbeitung
+      setTimeout(() => {
+        this.channelDescriptionInput.nativeElement.focus();
+      }, 0);
+    }
+  }
+
+  onSubmit(ngForm: NgForm) {}
 }
