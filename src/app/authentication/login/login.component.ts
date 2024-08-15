65810276fc569da68afe899cb395c4f37c0a4ac1
Laurin Herteux <156675458+Herteux94@users.noreply.github.com>
2024-08-15 15:57:02 +0200

    added authentication process login
    missing features:
    
    - error messages

--- src/app/authentication/login/login.component.ts
+++ src/app/authentication/login/login.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { AuthenticationComponent } from '../authentication.component';
 import { FocusInputDirective } from '../../directives/focus-input.directive';
+import { Auth, signInWithEmailAndPassword } from '@angular/fire/auth';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-login',
@@ -16,4 +19,25 @@ import { FocusInputDirective } from '../../directives/focus-input.directive';
 })
 export class LoginComponent {
 
+  errorMessage: string | null = null;
+
+  constructor(private auth: Auth, private router: Router) {}
+
+  login(email: string, password: string) {
+    if (!email || !password) {
+      this.errorMessage = 'Bitte geben Sie sowohl Ihre E-Mail-Adresse als auch Ihr Passwort ein.';
+      return;
+    }
+
+    signInWithEmailAndPassword(this.auth, email, password)
+      .then((userCredential) => {
+        console.log('User logged in:', userCredential.user);
+        this.errorMessage = null;
+        this.router.navigate(['/messenger']); // Weiterleitung zu /messenger nach erfolgreicher Anmeldung
+      })
+      .catch((error) => {
+        console.error('Error logging in:', error);
+        this.errorMessage = 'Fehler bei der Anmeldung. Bitte versuchen Sie es erneut.';
+      });
+  }
 }
