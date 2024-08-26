1cd5f6e52995650dbb73e99ef4a560b782951e27
Laurin Herteux <156675458+Herteux94@users.noreply.github.com>
2024-08-26 17:25:05 +0200

    added ability to download the files out of the channel

--- src/app/shared/messengerSubComponents/message/message.component.ts
+++ src/app/shared/messengerSubComponents/message/message.component.ts
@@ -1,23 +1,23 @@
 import { Component, inject, Input } from '@angular/core';
 import { Router, RouterLink, RouterLinkActive, RouterModule } from '@angular/router';
 import { ProfileDialogComponent } from '../../../dialogs/profile-dialog/profile-dialog.component';
-import { Dialog, DIALOG_DATA, DialogModule } from '@angular/cdk/dialog';
+import { Dialog, DialogModule } from '@angular/cdk/dialog';
 import { RoutingThreadOutletService } from '../../../services/routing-thread-outlet.service';
 import { ScreenSizeService } from '../../../services/screen-size-service.service';
 import { CommonModule } from '@angular/common';
-import { MAT_DIALOG_DATA, MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-message',
   standalone: true,
   imports: [RouterLink, RouterLinkActive, RouterModule, CommonModule, MatDialogModule, DialogModule],
   templateUrl: './message.component.html',
-  styleUrl: './message.component.scss'
+  styleUrls: ['./message.component.scss']
 })
 export class MessageComponent {
   mobile!: boolean;
   dialog = inject(Dialog);
-  
+
   @Input() isChannel!: boolean;
   @Input() ownMessage!: boolean;
   @Input() message!: {
@@ -31,19 +31,25 @@ export class MessageComponent {
   };
 
   constructor(
-    public threadRoutingService: RoutingThreadOutletService, 
-    private screenSizeService: ScreenSizeService, private router: Router
+    public threadRoutingService: RoutingThreadOutletService,
+    private screenSizeService: ScreenSizeService,
+    private router: Router
   ) {}
-
-  openProfileDialog() {
-    this.dialog.open(ProfileDialogComponent, {
-      data: { userID: this.message.senderID  }
-    });
-  }
 
   ngOnInit() {
     this.screenSizeService.isMobile().subscribe((isMobile) => {
       this.mobile = isMobile;
+    });
+
+    // Filtere ungültige URLs aus den Anhängen heraus
+    if (this.message.attachments) {
+      this.message.attachments = this.message.attachments.filter(url => url && url.trim() !== '');
+    }
+  }
+
+  openProfileDialog() {
+    this.dialog.open(ProfileDialogComponent, {
+      data: { userID: this.message.senderID }
     });
   }
 
@@ -56,5 +62,4 @@ export class MessageComponent {
 
     this.threadRoutingService.openThread();
   }
-
 }
