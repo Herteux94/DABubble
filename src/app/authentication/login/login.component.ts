e1905274c6a70b2ebbe737afae00f7c3fe35b58d
Laurin Herteux <156675458+Herteux94@users.noreply.github.com>
2024-08-29 12:39:49 +0200

    added guest login with prepared account

--- src/app/authentication/login/login.component.ts
+++ src/app/authentication/login/login.component.ts
@@ -1,7 +1,7 @@
-import { Component, HostListener, ViewChild, ElementRef } from '@angular/core';
-import { Router, RouterLink, RouterLinkActive, RouterModule } from '@angular/router';
+import { Component, HostListener } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 import { Auth, signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider } from '@angular/fire/auth';
-import { Firestore, doc, getDoc, setDoc } from '@angular/fire/firestore';
+import { Firestore, doc, getDoc } from '@angular/fire/firestore';
 import { CommonModule } from '@angular/common';
 import { ActiveUserService } from '../../services/active-user.service';
 import { FormsModule } from '@angular/forms';
@@ -12,7 +12,7 @@ import { FocusInputDirective } from '../../directives/focus-input.directive';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, RouterLink, RouterLinkActive, RouterModule, FormsModule, FocusInputDirective],
+  imports: [CommonModule, RouterModule, FormsModule, FocusInputDirective],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
@@ -34,7 +34,7 @@ export class LoginComponent {
   // Methode für E-Mail/Passwort-Login
   login() {
     if (!this.email) {
-      this.errorMessage = 'Bitte deine E-Mail-Adresse ein.';
+      this.errorMessage = 'Bitte gib deine E-Mail-Adresse ein.';
       this.errorType = 'email';
       return;
     }
@@ -68,28 +68,34 @@ export class LoginComponent {
       });
   }
 
-// Methode für Google-Login
-loginWithGoogle() {
-  const provider = new GoogleAuthProvider();
-  signInWithPopup(this.auth, provider)
-    .then(async (result) => {
-      const activeUserID = result.user.uid;
-      const displayName = result.user.displayName ?? ''; // Verwende einen leeren String, wenn displayName null oder undefined ist
-      const email = result.user.email ?? ''; // Verwende einen leeren String, wenn email null oder undefined ist
+  // Methode für Google-Login
+  loginWithGoogle() {
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(this.auth, provider)
+      .then(async (result) => {
+        const activeUserID = result.user.uid;
+        const displayName = result.user.displayName ?? ''; // Verwende einen leeren String, wenn displayName null oder undefined ist
+        const email = result.user.email ?? ''; // Verwende einen leeren String, wenn email null oder undefined ist
 
-      await this.checkOrCreateUserProfile(activeUserID, displayName, email);
-      this.errorMessage = '';
-      this.errorType = null;
-      this.activeUserService.loadActiveUser(activeUserID);  // Setze den aktiven Benutzer
-      this.router.navigate(['/messenger']);
-    })
-    .catch((error) => {
-      console.error('Error during Google sign-in:', error);
-      this.errorMessage = 'Fehler bei der Anmeldung mit Google. Bitte versuchen Sie es erneut.';
-      this.errorType = null;
-    });
-}
+        await this.checkOrCreateUserProfile(activeUserID, displayName, email);
+        this.errorMessage = '';
+        this.errorType = null;
+        this.activeUserService.loadActiveUser(activeUserID);  // Setze den aktiven Benutzer
+        this.router.navigate(['/messenger']);
+      })
+      .catch((error) => {
+        console.error('Error during Google sign-in:', error);
+        this.errorMessage = 'Fehler bei der Anmeldung mit Google. Bitte versuchen Sie es erneut.';
+        this.errorType = null;
+      });
+  }
 
+  // Methode für Gäste-Login
+  guestLogin() {
+    this.email = 'info@herteux-webentwicklung.de';
+    this.password = 'Bewerbungen2024';
+    this.login();
+  }
 
   async checkOrCreateUserProfile(activeUserID: string, displayName?: string, email?: string) {
     const userRef = doc(this.firestore, `users/${activeUserID}`);
@@ -104,7 +110,7 @@ loginWithGoogle() {
 
       try {
         await this.firestoreService.addUser(user.toJSON());
-        console.log('Neues Benutzerprofil in Firestore erstellt: ', user);
+        console.log('Neues Benutzerprofil in Firestore erstellt:', user);
       } catch (error) {
         console.error('Fehler beim Erstellen des Benutzerprofils in Firestore:', error);
       }
