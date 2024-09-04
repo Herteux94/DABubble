c849a5ab562d530150488d067489b144ee23f3a8
RobinWestmeier <146763348+RobinWestmeier@users.noreply.github.com>
2024-09-04 14:54:47 +0200

    Update active-channel.service.ts

--- src/app/services/active-channel.service.ts
+++ src/app/services/active-channel.service.ts
@@ -8,62 +8,6 @@ import { ActiveUserService } from './active-user.service';
   providedIn: 'root',
 })
 export class ActiveChannelService {
-  // activeChannel: any;
-  // channelMessages$!: Observable<any[]>;
-  // channelMessages: Message[] = [];
-
-  // constructor(
-  //   private firestoreService: FirestoreService,
-  //   private activeUserService: ActiveUserService
-  // ) {}
-
-  // async loadActiveChannelAndMessages(channelID: string) {
-  //   await this.loadActiveChannel(channelID);
-  //   this.loadChannelMessages(channelID);
-  // }
-
-  // async loadActiveChannel(channelID: string): Promise<void> {
-  //   this.activeUserService.activeUserChannels$
-  //     .pipe(
-  //       first((channels) => channels.length > 0),
-  //       map((channels) =>
-  //         channels.find((channel) => channel.channelID === channelID)
-  //       )
-  //     )
-  //     .subscribe({
-  //       next: (channel) => {
-  //         if (channel) {
-  //           this.activeChannel = channel;
-  //         } else {
-  //           console.error('Channel nicht gefunden');
-  //         }
-  //       },
-  //       error: (error) => {
-  //         console.error('Fehler beim Laden des aktiven Channels:', error);
-  //       },
-  //     });
-  // }
-
-  // loadChannelMessages(channelID: string) {
-  //   this.channelMessages$ = this.firestoreService.getMessages(
-  //     'channels',
-  //     channelID
-  //   );
-  //   this.channelMessages$.subscribe({
-  //     next: (messages) => {
-  //       if (messages) {
-  //         this.channelMessages = messages.sort(
-  //           (a, b) => a.creationTime - b.creationTime
-  //         );
-  //       } else {
-  //         console.error('Messages nicht gefunden');
-  //       }
-  //     },
-  //     error: (error) => {
-  //       console.error('Fehler beim Laden der aktiven Messages:', error);
-  //     },
-  //   });
-  // }
 
   private activeChannelSubject = new BehaviorSubject<any>(null); // Initial null value
   activeChannel$ = this.activeChannelSubject.asObservable(); // Expose as observable
