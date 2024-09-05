7aa9c947fd4dd1e484d8302f1e495bc2f7b300fc
Kytez <161442764+Kytez@users.noreply.github.com>
2024-09-05 13:05:59 +0200

    updated threadRoutinService

--- src/app/services/routing-thread-outlet.service.ts
+++ src/app/services/routing-thread-outlet.service.ts
@@ -1,34 +1,24 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-// import { ActiveChannelService } from './active-channel.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class RoutingThreadOutletService {
   threadOpenDesktop!: boolean;
-  // activeThreadChannelName!: string;
 
-  constructor(
-    private router: Router,
-    private route: ActivatedRoute // private activeChannelService: ActiveChannelService
-  ) {}
+  constructor(private router: Router, private route: ActivatedRoute) {}
 
   closeThread() {
-    // this.threadOpenDesktop = false;
-    // setTimeout(() => {
-    //   this.router.navigate(['/messenger', { outlets: { thread: null } }], {
-    //     relativeTo: this.route.parent,
-    //   });
-    // }, 350);
+    this.threadOpenDesktop = false;
+    setTimeout(() => {
+      this.router.navigate(['/messenger', { outlets: { thread: null } }], {
+        relativeTo: this.route.parent,
+      });
+    }, 350);
   }
 
   openThread() {
     this.threadOpenDesktop = true;
-    // this.loadActiveThreadChannelName();
   }
-
-  // loadActiveThreadChannelName() {
-  //   this.activeThreadChannelName = this.activeChannelService.activeChannel.name;
-  // }
 }
