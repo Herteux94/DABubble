f8e1afb794a2bffdb16692631728b7db9498e73e
RobinWestmeier <146763348+RobinWestmeier@users.noreply.github.com>
2024-09-05 11:05:36 +0200

    Update thread.component.ts

--- src/app/main-content/thread/thread.component.ts
+++ src/app/main-content/thread/thread.component.ts
@@ -31,9 +31,10 @@ export class ThreadComponent implements OnInit {
         threadID = paramMap.get('id');
       });
 
-      this.threadRoutingService.openThread(); // if Abfrage wegen doppelter Animation
-
-      this.activeThreadService.loadActiveThreadAndMessages(threadID);
+      setTimeout(() => {
+        this.threadRoutingService.threadOpenDesktop = true; // if-Abfrage wegen doppelter Animation
+        this.activeThreadService.loadActiveThreadAndMessages(threadID);
+      }, 0);  // Delay für die nächste Change Detection
     }
   }
 }
