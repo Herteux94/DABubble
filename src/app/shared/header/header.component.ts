9bf65311c8b71c7893f1d4d493cb4e2c2008917e
RobinWestmeier <146763348+RobinWestmeier@users.noreply.github.com>
2024-09-11 13:36:20 +0200

    added header search query

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import {
   ActivatedRoute,
   Router,
@@ -12,6 +12,8 @@ import { MenuDialogComponent } from '../../dialogs/menu-dialog/menu-dialog.compo
 import { ActiveUserService } from '../../services/active-user.service';
 import { CommonModule } from '@angular/common';
 import { User } from '../../models/user.model';
+import { FirestoreService } from '../../services/firestore.service';
+import { ProfileDialogComponent } from '../../dialogs/profile-dialog/profile-dialog.component';
 
 @Component({
   selector: 'app-header',
@@ -22,15 +24,22 @@ import { User } from '../../models/user.model';
 })
 export class HeaderComponent implements OnInit {
   dialog = inject(Dialog);
+  activeUserService = inject(ActiveUserService);
+  firestoreService = inject(FirestoreService);
   mobile!: boolean;
   navigationCompActive: boolean = true;
   avatarUrl: string = '../../../assets/img/avatars/avatar-4.svg';
+  user!: User[];
+  dialogOpen = false;
+  headerSearchQuery = signal('');
+  filteredChannels: any[] = [];
+  filteredUsers: any[] = [];
+  searchListOpen = false;
 
   constructor(
     private screenSizeService: ScreenSizeService,
     private router: Router,
-    private activatedRoute: ActivatedRoute,
-    public activeUserService: ActiveUserService
+    private activatedRoute: ActivatedRoute
   ) {}
 
   ngOnInit() {
@@ -43,8 +52,62 @@ export class HeaderComponent implements OnInit {
     });
   }
 
+  onHeaderSearchInput(event: Event): void {
+    const searchTerm = (event.target as HTMLInputElement).value.toLowerCase();
+    this.headerSearchQuery.set(searchTerm);
+    this.filterResults(searchTerm);
+  }
+
+  onInputFocus() {
+    this.searchListOpen = true;
+  }
+
+  onInputBlur() {
+    setTimeout(() => {
+      this.searchListOpen = false;
+      this.headerSearchQuery.set("");
+    }, 200); // Verzögerung, um sicherzustellen, dass der Klick auf das Ergebnis registriert wird
+  }
+
+  filterResults(searchTerm: string) {
+    this.filteredChannels = this.activeUserService.activeUserChannels
+      .filter(channel => channel.name.toLowerCase().includes(searchTerm))
+      .sort((a, b) => a.name.localeCompare(b.name));
+
+    this.filteredUsers = this.firestoreService.allUsers
+      .filter(user => user.name.toLowerCase().includes(searchTerm))
+      .sort((a, b) => a.name.localeCompare(b.name));
+  }
+
+  navigateToChannel(channelID: any) {
+    this.router.navigate([`messenger/channel/${channelID}`]);
+  }
+
+  // onHeaderSearchInput(event: Event): void {
+  //   const inputElement = event.target as HTMLInputElement;
+  //   const inputValue = inputElement.value;
+  //   this.dialogOpen = false;
+  //   this.headerSearchQuery.set(inputValue);
+
+  //   if (inputValue.startsWith('@')) {
+  //     this.firstLetter.set('@');
+  //   } else if (inputValue.startsWith('#')) {
+  //     this.firstLetter.set('#');
+  //   } else {
+  //     this.firstLetter.set('');
+  //   }
+  // }
+
   openMenuDialog() {
     this.dialog.open(MenuDialogComponent, {});
+  }
+
+  openProfileDialog(user:User) {
+    this.dialog.open(ProfileDialogComponent, {
+      data: { userID: user.userID },
+    });
+    this.dialogOpen = true;
+    this.headerSearchQuery.set("");
   }
 
   navigateToStart() {
